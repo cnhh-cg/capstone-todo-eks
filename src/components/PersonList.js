5c5235f5f8e4a0a504c752bc889963dcb842483a
Chanh Hua <Chanh.Hua@capgroup.com>
2023-02-17 10:40:40 -0800

    initial

---
+++ src/components/PersonList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://pz8ivsp57g.execute-api.us-west-2.amazonaws.com/development`)
+      .then(res => {
+        const persons = res.data.body;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.state.persons
+            .map(person =>
+              <li key={person.id}>{person.id}{person.name}</li>
+            )
+        }
+      </ul>
+    )
+  }
+}
